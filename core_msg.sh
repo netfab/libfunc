0f565dcd71711b14a8e3120c95a2cc992e8dc2af
netfab <netbox253@gmail.com>
2017-05-23 10:24:40 +0200

    Use only one variable to declare and define the log system.

--- core_msg.sh
+++ core_msg.sh
@@ -1,33 +1,31 @@
 #!/bin/bash
 
-declare LOGSYSTEM='off'
-
 function fn_logs_init() {
 	local -r programnameerror='You must set the ${programname} bash variable.'
-	local -r logtypeerror='You must set the ${logtype} bash variable.'
+	local -r logsystemerror='You must set the ${logsystem} bash variable.'
 
 	local -r defaultlogrootdir='/var/tmp'
 	local -r defaultlogfile="${programname:?${programnameerror}}-script.log"
 
-	case "${logtype:?${logtypeerror}}" in
+	case "${logsystem:?${logsystemerror}}" in
 		'own'|'system'|'systemd')
-			LOGSYSTEM='on'
+			printf "log system : ${logsystem}\n"
+		;;
+		'off')
+			printf "log system disabled by configuration request\n"
 		;;
 		*)
-			LOGSYSTEM='off'
-			fn_exit_with_fatal_error "wrong \${logtype} value : ${logtype}"
+			local -r tmplog="${logsystem}"
+			logsystem='off'
+			fn_exit_with_fatal_error "wrong \${logsystem} value : ${tmplog}"
 		;;
 	esac
 
-	if [ "${LOGSYSTEM}" == 'on' ]; then
-		printf "log system : ${logtype}\n"
-	fi
-
-	if [ "${logtype}" == 'own' ]; then
+	if [ "${logsystem}" == 'own' ]; then
 		if [ ! -d "${logrootdir:=${defaultlogrootdir}}/${programname}" ]; then
 			mkdir -p "${logrootdir}/${programname}"
 			if [ $? -ne 0 ]; then
-				LOGSYSTEM='off'
+				logsystem='off'
 				fn_exit_with_fatal_error 'making log directory failed !'
 			fi
 		fi
@@ -38,7 +36,7 @@ function fn_logs_init() {
 			printf "rotating log file : "
 			mv "${logrootdir}/${logfile}" "${logrootdir}/${logfile/.log/}-${daterun}.log"
 			if [ $? -ne 0 ]; then
-				LOGSYSTEM='off'
+				logsystem='off'
 				fn_exit_with_fatal_error 'log file can not be rotated !'
 			fi
 			fn_print_status_ok_eol
@@ -46,7 +44,8 @@ function fn_logs_init() {
 
 		printf "log file : ${logrootdir}/${logfile}\n"
 	else
-		printf "${logtype}\n"
+		# TODO
+		printf "${logsystem}\n"
 	fi
 }
 
@@ -55,7 +54,7 @@ function fn_print_status_ok_eol() {
 }
 
 function fn_print_msg() {
-	if [ "${LOGSYSTEM}" == 'on' ]; then
+	if [ "${logsystem}" != 'off' ]; then
 		fn_log "${@}"
 	fi
 	printf "${@}\n"
