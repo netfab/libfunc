70518de5e85a6e69c9bb11330fb006b15f4ef8f0
netfab <netbox253@gmail.com>
2017-05-23 16:41:18 +0200

    Double check arguments.

--- core_getopts.sh
+++ core_getopts.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
+function fn_check_option_argument() {
+	local -r opt=$1
+	local -r arg=$2
+	if [ "${arg:0:1}" == '-' ]; then
+			fn_exit_with_fatal_error "Option -${opt} requires an argument."
+	fi
+}
+
 while getopts ":a:b:" opt; do
 	case $opt in
     	a)
 			echo "-a was triggered, Parameter: $OPTARG"
+			fn_check_option_argument $opt $OPTARG
 		;;
     	b)
 			echo "-b was triggered, Parameter: $OPTARG"
+			fn_check_option_argument $opt $OPTARG
 		;;
 		\?)
 			fn_exit_with_fatal_error "Invalid option: -$OPTARG"
