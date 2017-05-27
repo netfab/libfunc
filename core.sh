e3f39e023bad6e23201b561f5750ffed5c85550b
netfab <netbox253@gmail.com>
2017-05-27 16:42:13 +0200

    Implement function to run command.

--- core.sh
+++ core.sh
@@ -3,7 +3,7 @@
 export LANG=C
 
 source "${libfuncdir}/core_exit.sh"
-source "${libfuncdir}/core_command.sh"
 source "${libfuncdir}/core_msg.sh"
 source "${libfuncdir}/core_getopts.sh"
+source "${libfuncdir}/core_command.sh"
 
--- core_command.sh
+++ core_command.sh
@@ -7,4 +7,25 @@ function fn_check_last_status_fatal() {
 	fi
 }
 
+function fn_run_command() {
+	fn_log "running command: $@"
+
+	case "${logsystem}" in
+		'own')
+			(eval $@ 2>&1) >> "${logrootdir}/${logfile}"
+			;;
+		'system')
+			# TODO
+			;;
+		'systemd')
+			# TODO
+			;;
+		'off')
+			;;
+	esac
+
+	local ret=$?
+	fn_log "command exited with status: ${ret}"
+	return ${ret}
+}
 
