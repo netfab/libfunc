43ec027f95052603a0ab49f73bc29b9786fdece7
netfab <netbox253@gmail.com>
2017-05-27 12:44:32 +0200

    Implement getopts initialization functions.

--- core_getopts.sh
+++ core_getopts.sh
@@ -1,29 +1,88 @@
 #!/bin/bash
 
-function fn_check_option_argument() {
-	local -r opt=$1
-	local -r arg=$2
-	if [ "${arg:0:1}" == '-' ]; then
-			fn_exit_with_fatal_error "Option -${opt} requires an argument."
-	fi
+declare -A options
+
+function fn_getopts_check_options() {
+	local knownopt x s reqarg opt longopt
+
+	while [ $# -ne 0 ]; do
+		knownopt='unknown'
+		#printf "%s\n" "$1"
+		for x in ${programoptions}; do
+			reqarg='off'
+			opt=${x%%,*}
+			longopt=${x#*,}
+			s=1
+			if [ "${x:(-1)}" == ':' ]; then
+				reqarg='on'
+				longopt=${longopt:0:(-1)}
+				s=2
+			fi
+			if [ "${1}" == "-${opt}" ] || [ "${1}" == "--${longopt}" ]; then
+				knownopt='known'
+				break
+			fi
+		done
+
+		if [ "${knownopt}" == 'unknown' ]; then
+			fn_exit_with_fatal_error "unknown option : ${1}"
+		fi
+
+		shift $s
+	done
 }
 
-while getopts ":a:b:" opt; do
-	case $opt in
-    	a)
-			echo "-a was triggered, Parameter: $OPTARG"
-			fn_check_option_argument $opt $OPTARG
-		;;
-    	b)
-			echo "-b was triggered, Parameter: $OPTARG"
-			fn_check_option_argument $opt $OPTARG
-		;;
-		\?)
-			fn_exit_with_fatal_error "Invalid option: -$OPTARG"
-			;;
-		:)
-			fn_exit_with_fatal_error "Option -$OPTARG requires an argument."
-			;;
-   	esac
-done
+function fn_getopts_init() {
+	local -r optionserror='You must set the ${programoptions} bash variable.'
+
+	local x y reqarg opt longopt
+	for x in ${programoptions:?${optionserror}}; do
+		reqarg='off'
+		opt=${x%%,*}
+		longopt=${x#*,}
+		if [ "${x:(-1)}" == ':' ]; then
+			reqarg='on'
+			longopt=${longopt:0:(-1)}
+		fi
+
+		options["${longopt}"]='off'
+		#printf "$opt $longopt $reqarg\n"
+
+		for y in $@; do
+			#printf "%s %s\n" "${longopt} $y"
+
+			if [ ${options["${longopt}"]} != 'off' ]; then
+				if [ "${reqarg}" == 'on' ]; then
+					if [ "${y:0:1}" == '-' ]; then
+						fn_exit_with_fatal_error "Option --${longopt} requires an argument."
+					fi
+					options["${longopt}"]="${y}"
+					reqarg='off'
+				fi
+
+				#printf "skipping ${longopt}\n"
+				continue
+			fi
+
+			if [ "${y}" == "-${opt}" ] || [ "${y}" == "--${longopt}" ]; then
+				options["${longopt}"]='on'
+				continue
+			fi
+		done
+
+		if [ "${reqarg}" == 'on' ] && [ "${options["${longopt}"]}" == 'on' ]; then
+			fn_exit_with_fatal_error "Option --${longopt} requires an argument."
+		fi
+	done
+
+	fn_getopts_check_options $@
+	unset -f fn_getopts_check_options
+}
+
+fn_getopts_init $@
+unset -f fn_getopts_init
+
+#for x in ${!options[@]}; do
+#	printf "$x - ${options[$x]}\n"
+#done
 
